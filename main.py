e522e2d9687a30a418bd613f2916f164ad375b61
Eric Snyder <guideon72@hotmail.com>
2023-02-28 10:04:03 -0800

    Wired in Search dialog functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import sqlite3
 from pathlib import Path
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (
     QApplication,
@@ -28,6 +29,11 @@ class MainWindow(QMainWindow):
         add_student_action = QAction("Add Student", self)
         add_student_action.triggered.connect(self.insert_student)
         file_menu_item.addAction(add_student_action)
+
+        edit_menu_item = self.menuBar().addMenu("&Edit")
+        find_student_action = QAction("Search", self)
+        find_student_action.triggered.connect(self.find_student)
+        edit_menu_item.addAction(find_student_action)
 
         help_menu_item = self.menuBar().addMenu("&Help")
         help = QAction("Get Help", self)
@@ -57,6 +63,10 @@ class MainWindow(QMainWindow):
         dialog = InsertDialog(self.load_data)
         dialog.exec()
 
+    def find_student(self):
+        dialog = FindDialog(self.table)
+        dialog.exec()
+
 
 class InsertDialog(QDialog):
     def __init__(self, reload):
@@ -69,26 +79,29 @@ class InsertDialog(QDialog):
 
         layout = QVBoxLayout()
 
+        # Set up student name field
         stdnt_name = QLabel("Name")
         self.stdnt_edit_line = QLineEdit()
         self.stdnt_edit_line.setPlaceholderText("Name")
         layout.addWidget(stdnt_name)
         layout.addWidget(self.stdnt_edit_line)
 
+        # Setup class name combo box
         class_name = QLabel("Class")
         self.class_name = QComboBox()
         courses = ["Biology", "Math", "Astronomy", "Physics"]
         self.class_name.addItems(courses)
-
         layout.addWidget(class_name)
         layout.addWidget(self.class_name)
 
+        # Set up student mobile number field
         phone_num = QLabel("Phone")
         self.phone_edit_line = QLineEdit()
         self.phone_edit_line.setPlaceholderText("Mobile number")
         layout.addWidget(phone_num)
         layout.addWidget(self.phone_edit_line)
 
+        # Set up the Submit button
         submit = QPushButton("Submit Student")
         submit.clicked.connect(self.add_student)
         layout.addWidget(submit)
@@ -117,6 +130,55 @@ class InsertDialog(QDialog):
         curs.close()
         connection.close()
         self.load_data()
+        self.close()
+
+
+class FindDialog(QDialog):
+    def __init__(self, mwtable):
+        super().__init__()
+        self.search_table = mwtable
+
+        self.setWindowTitle("Find Student Record")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Set up student name field
+        stdnt_name = QLabel("Name")
+        self.stdnt_search_line = QLineEdit()
+        self.stdnt_search_line.setPlaceholderText("Find Name")
+        layout.addWidget(stdnt_name)
+        layout.addWidget(self.stdnt_search_line)
+
+        # Set up the Search button
+        search = QPushButton("Find Student")
+        search.clicked.connect(self.find_student)
+        layout.addWidget(search)
+
+        # Displays the layout
+        self.setLayout(layout)
+
+    def find_student(self):
+        base = Path(__file__).parent / "data"
+        db_path = Path(base / "database.db")
+        connection = sqlite3.connect(db_path)
+        curs = connection.cursor()
+
+        name = self.stdnt_search_line.text()
+
+        results = curs.execute("SELECT * FROM students WHERE name = ?", (name,))
+        rows = list(results)
+        # print(rows)
+        items = self.search_table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        # clear current selection.
+        self.search_table.setCurrentItem(None)
+        for item in items:
+            # print(item.text())
+            self.search_table.item(item.row(), 1).setSelected(True)
+
+        curs.close()
+        connection.close()
         self.close()
 
 
