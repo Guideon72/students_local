1eb28f1ba94659ce66430e37d79b01b990825610
Eric Snyder <guideon72@hotmail.com>
2023-02-27 17:55:33 -0800

    Current state of main.py; still have bug on main dialog not refreshing

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
+    QDialog,
     QLabel,
     QTableWidget,
     QTableWidgetItem,
-    QGridLayout,
+    QVBoxLayout,
+    QComboBox,
     QLineEdit,
     QPushButton,
 )
@@ -18,11 +20,14 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Mangement System")
+        self.setFixedHeight(400)
+        self.setFixedWidth(425)
 
         # Build menu bar options
         file_menu_item = self.menuBar().addMenu("&File")
-        add_student = QAction("Add Student", self)
-        file_menu_item.addAction(add_student)
+        add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert_student)
+        file_menu_item.addAction(add_student_action)
 
         help_menu_item = self.menuBar().addMenu("&Help")
         help = QAction("Get Help", self)
@@ -34,6 +39,7 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(("ID", "NAME", "COURSE", "PHONE"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+        self.load_data()
 
     def load_data(self):
         base = Path(__file__).parent / "data"
@@ -47,7 +53,67 @@ class MainWindow(QMainWindow):
                 self.table.setItem(row_num, col_num, QTableWidgetItem(str(data)))
         connection.close()
 
-        # self.table
+    def insert_student(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Record")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        stdnt_name = QLabel("Name")
+        self.stdnt_edit_line = QLineEdit()
+        self.stdnt_edit_line.setPlaceholderText("Name")
+        layout.addWidget(stdnt_name)
+        layout.addWidget(self.stdnt_edit_line)
+
+        class_name = QLabel("Class")
+        self.class_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.class_name.addItems(courses)
+
+        layout.addWidget(class_name)
+        layout.addWidget(self.class_name)
+
+        phone_num = QLabel("Phone")
+        self.phone_edit_line = QLineEdit()
+        self.phone_edit_line.setPlaceholderText("Mobile number")
+        layout.addWidget(phone_num)
+        layout.addWidget(self.phone_edit_line)
+
+        submit = QPushButton("Submit Student")
+        submit.clicked.connect(self.add_student)
+        layout.addWidget(submit)
+
+        # Displays the layout
+        self.setLayout(layout)
+
+    def add_student(self):
+        base = Path(__file__).parent / "data"
+        db_path = Path(base / "database.db")
+        connection = sqlite3.connect(db_path)
+
+        name = self.stdnt_edit_line.text()
+        course = self.class_name.itemText(self.class_name.currentIndex())
+        phone = self.phone_edit_line.text()
+        curs = connection.cursor()
+        curs.execute(
+            "INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+            (
+                name,
+                course,
+                phone,
+            ),
+        )
+        connection.commit()
+        curs.close()
+        connection.close()
 
 
 def main():
